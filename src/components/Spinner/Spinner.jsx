10c7a29155c119c7ebc49fb1647ed66528163e7e
Gabriel Uviedo <gabriel.uviedo@sarsy-srl.com>
2020-08-09 22:30:27 -0300

    Agrego spinner de carga

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import './Spinner.css'
 export default function Spinner() {
-  return <>
-    <i>Cargando...</i>;
-  </>;
+  return (
+    <>
+      <div class="spinner-box">
+        <div class="solar-system">
+          <div class="earth-orbit orbit">
+            <div class="planet earth"></div>
+            <div class="venus-orbit orbit">
+              <div class="planet venus"></div>
+              <div class="mercury-orbit orbit">
+                <div class="planet mercury"></div>
+                <div class="sun"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { useGifs } from "../../hooks/useGifs";
 
 export default function HomePage() {
+  const { loading, gifs } = useGifs({ keyword: "" });
   return <></>;
 }
