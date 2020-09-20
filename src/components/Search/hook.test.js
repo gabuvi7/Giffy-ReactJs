1d6afed79f6e4e045392736ecee9775c129e10fa
Gabriel Uviedo <gabriel.uviedo@sarsy-srl.com>
2020-09-19 23:14:59 -0300

    Se agregan mas test

--- src/components/Search/hook.test.js
+++ src/components/Search/hook.test.js
@@ -8,5 +8,30 @@ test("should change keyword", () => {
   act(() => {
     result.current.updateKeyword(KEYWORD);
   });
+
   expect(result.current.keyword).toBe(KEYWORD);
+});
+
+test("should use initial values", () => {
+  const { result } = renderHook(() =>
+    useForm({
+      initialKeyword: "avengers",
+    })
+  );
+  
+  expect(result.current.keyword).toBe("avengers");
+});
+
+test("should update correctly times when used twice", () => {
+  const { result } = renderHook(() => 
+    useForm({
+      initialKeyword: KEYWORD,
+    })
+  );
+  act(() => {
+    result.current.updateKeyword("B");
+    result.current.updateKeyword("Ba");
+  });
+  expect(result.current.keyword).toBe("Ba");
+  expect(result.current.times).toBe(2)
 });
